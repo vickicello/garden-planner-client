53374ef3cdf9a290e7647c3033ef109bfeef2563
vickicello <vicki.cello@gmail.com>
2019-03-05 16:26:36 -0500

    update styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import harvest from '../harvest.jpg';
 
 const Home = () => {
   return (
     <div className="Home">
       <div className="HomeText">
         <h1>Welcome to Garden Planner!</h1>
+        <img src={harvest} width="40%" height="65%" alt="harvest" />
+
         <p>Navigate this site and build the garden of your dreams.</p>
         <p>Keep track of sow by dates, harvest times, and all of your favorite varieties.</p>
       </div>
